038a5651a154b7276c7fdfe15d9e20a14ea9054e
Nick <pretki123@ukr.net>
2023-10-25 14:14:36 -0600

    Setup project

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Nick Pavlov on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
