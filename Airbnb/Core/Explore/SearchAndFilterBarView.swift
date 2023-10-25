616187cda23956a8782234a3756014d0c1f8ace6
Nick <pretki123@ukr.net>
2023-10-25 15:08:42 -0600

    searchBar created and inserted in ExploreView

---
+++ Airbnb/Core/Explore/SearchAndFilterBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchAndFilterBarView.swift
+//  Airbnb
+//
+//  Created by Nick Pavlov on 10/25/23.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchAndFilterBarView()
+}
