f68ddd1a05ffde9724359ee56e1fe426f545ddeb
Nick <pretki123@ukr.net>
2023-10-25 14:24:33 -0600

    project was set up

---
+++ Airbnb/Core/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  Airbnb
+//
+//  Created by Nick Pavlov on 10/25/23.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
