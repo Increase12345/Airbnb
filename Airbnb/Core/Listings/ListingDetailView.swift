7ee2d85d2a2c1fe0c075dd1884a3be306fd95198
Nick <pretki123@ukr.net>
2023-10-26 13:41:57 -0600

    listing detain view part 1 done

---
+++ Airbnb/Core/Listings/ListingDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingDetailView.swift
+//  Airbnb
+//
+//  Created by Nick Pavlov on 10/26/23.
+//
+
+import SwiftUI
+
+struct ListingDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingDetailView()
+}
